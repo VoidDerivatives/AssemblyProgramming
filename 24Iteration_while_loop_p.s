59d89724dd6f7d71cd5e519e50deb3473fe92e78
VoidDerivatives <aishwarya.bacche28@gmail.com>
2021-08-27 19:55:26 +0530

    While loop

---
+++ 24Iteration_while_loop_p.s
@@ -0,0 +1,49 @@
+.section .rodata
+	msg_main_print:
+	.string "Enter the numbers:\t"
+	
+	msg_main_scan:
+	.string "%d"
+
+	msg_main_print1:
+	.string "%d\n"
+
+.section .rodata 
+.globl main
+.type main,@function
+main:
+	pushl %ebp
+	movl %esp,%ebp
+
+	subl $8,%esp
+
+	pushl $msg_main_print
+	call printf
+	addl $4,%esp
+
+	leal -4(%ebp),%eax
+	pushl %eax
+	pushl $msg_main_scan
+	call scanf
+	addl $8,%esp
+
+	movl $0,-8(%ebp)
+	
+	jmp label_condition
+
+main_for:
+	pushl %eax
+	pushl $msg_main_print1
+	call printf
+	addl $8,%esp
+	
+	addl $1,-8(%ebp)
+	
+label_condition:
+	movl -8(%ebp),%eax
+	movl -4(%ebp),%edx
+	cmpl %edx,%eax
+	jl  main_for
+
+	pushl $0
+	call exit
