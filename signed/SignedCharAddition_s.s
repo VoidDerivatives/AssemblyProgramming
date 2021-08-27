8b2aa794f6838b10bf4fadc64d4fd023a2470673
VoidDerivatives <aishwarya.bacche28@gmail.com>
2021-08-27 23:23:30 +0530

    Signed

---
+++ signed/SignedCharAddition_s.s
@@ -0,0 +1,40 @@
+.section .rodata
+	msg_main_print1:
+	.string "Overflow flag is set\n"
+
+	msg_main_print2:
+	.string "Overflow flag is not set\n"
+
+.section .text
+.globl main
+.type main,@function
+main:
+	pushl %ebp
+	movl %esp,%ebp
+	andl $-16,%esp
+
+	subl $16,%esp
+	
+	movb $-128,%al
+	movb $-1,%bl
+
+	addb %bl,%al
+	jo label_set
+
+	#flag is not set
+	movl $msg_main_print2,(%esp)
+	movb %al,4(%esp)
+	call printf
+
+	jmp label_exit
+label_set:
+	
+	#flag is set
+	movl $msg_main_print1,(%esp)
+	movb %al,4(%esp)
+	call printf
+
+label_exit:
+
+	movl $0,(%esp)
+	call exit
---
+++ signed/UnsignedCharsub_s.s
@@ -0,0 +1,41 @@
+.section .rodata
+	msg_main_print1:
+	.string "Carry flag is set\n"
+
+	msg_main_print2:
+	.string "Carry flag is not set\n"
+
+.section .text
+.globl main
+.type main,@function
+main:
+	pushl %ebp
+	movl %esp,%ebp
+	andl $-16,%esp
+
+	subl $16,%esp
+	
+	#movb $128,%al
+	#movb $128,%bl
+
+	movb $7,%al
+	movb $3,%bl
+
+	subb %bl,%al
+	jc label_set
+
+	#flag is not set
+	movl $msg_main_print2,(%esp)
+	call printf
+
+	jmp label_exit
+label_set:
+	
+	#flag is set
+	movl $msg_main_print1,(%esp)
+	call printf
+
+label_exit:
+
+	movl $0,(%esp)
+	call exit
