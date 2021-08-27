0c37f862999c23c0696dc07cdb6b43a80771147a
VoidDerivatives <aishwarya.bacche28@gmail.com>
2021-08-27 19:55:51 +0530

    Unconditional break

---
+++ 26uncondi_break_p.s
@@ -0,0 +1,63 @@
+.section .rodata
+	msg_main_print:
+	.string "Enter the numbers :\t"
+	
+	msg_main_scan:
+	.string "%d"
+
+	msg_main_print1:
+	.string "%d\n"	
+	
+.section .text
+.globl main
+.type main,@function
+main:
+	pushl %ebp
+	movl %esp,%ebp
+
+	subl $8,%esp
+
+	pushl $msg_main_print
+	call printf
+	addl $4,%esp
+
+	leal -4(%ebp),%ebx
+	pushl %ebx
+	pushl $msg_main_scan
+	call scanf
+	addl $8,%esp
+
+	movl $1,-8(%ebp)
+	jmp main_cond
+	
+main_for:
+
+	xorl %ecx,%ecx
+	xorl %edx,%edx
+	
+	movl $2,%ecx
+	divl %ecx
+		
+	cmpl $0,%edx
+	
+	je main_break	
+
+	movl -8(%ebp),%eax        #qotionent gela ahee 
+	pushl %eax
+	pushl $msg_main_print1
+	call printf
+	addl $8,%esp
+
+	addl $1,-8(%ebp)
+main_cond:
+
+ 	movl -8(%ebp),%eax
+	movl -4(%ebp),%edx
+	cmpl %edx,%eax
+	jl main_for	
+	
+main_break:
+	
+	pushl $0
+	call exit
+
